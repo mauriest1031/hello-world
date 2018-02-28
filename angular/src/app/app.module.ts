012ef9fc0c54258f8f1ba7884ed1248a924ebc10
mauriest1031 <mauriest1031@gmail.com>
2018-02-27 22:19:16 -0500

    Added login and registration components Added login and registration html forms

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -10,10 +10,14 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'blog', component: BlogComponent}
+  {path: 'blog', component: BlogComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
 ];
 
 @NgModule({
@@ -21,7 +25,9 @@ const appRoutes: Routes = [
     AppComponent,
     NavComponent,
     HomeComponent,
-    BlogComponent
+    BlogComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
